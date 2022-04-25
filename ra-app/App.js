677c60da18d4adef5d8927aa5ddd23d367502648
Daniel-Stoltzfus <dsstoltzfus@wesleyan.edu>
2022-04-25 15:24:31 -0400

    progrees on opening screen, grabbing data from back end, not displaying it.

--- ra-app/App.js
+++ ra-app/App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { FlatList, Text, View } from "react-native";
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/artist.json")
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 14,
+              color: "green",
+              textAlign: "center",
+              paddingBottom: 10,
+            }}
+          >
+          </Text>
+          <FlatList
+            data={data.artist}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <Text>{item.song + ", " + item.artist}</Text>
+            )}
+          />
+        </View>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
